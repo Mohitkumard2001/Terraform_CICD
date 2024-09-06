98c01a61071a7428100cb07c9cc7a1ea8b44d781
Mohitkumard2001 <doddimohitkumar@gmail.com>
2024-09-06 21:33:13 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-02b49a24cfb95941c"
+    ami = "ami-0e53db6fd757e38c7"
     instance_type = "t2.micro"
      key_name = "pair"
     tags = {
