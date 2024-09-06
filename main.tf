1164ffeb70ad765af9ce15f285b49926b4a12912
Mohitkumard2001 <doddimohitkumar@gmail.com>
2024-09-06 21:34:51 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "dev" {
     ami = "ami-0e53db6fd757e38c7"
     instance_type = "t2.micro"
-     key_name = "pair"
+     key_name = "keypair"
     tags = {
       Name = "test"
     }
